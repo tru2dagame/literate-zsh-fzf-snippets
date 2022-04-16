c8d6a8296f644ac84e6848f449287753b1667ab5
Tru <tru.zhou@gmail.com>
2022-04-16 08:32:00 +0800

    update default SNIPPETS_PATH

--- literate-zsh-fzf-snippets.zsh
+++ literate-zsh-fzf-snippets.zsh
@@ -1,6 +1,6 @@
 #!/usr/bin/env zsh
 
-SNIPPETS_PATH=${SNIPPETS_PATH:-"$HOME/.config/snippets"}
+SNIPPETS_PATH=${SNIPPETS_PATH:-"$HOME/.config/literate-zsh-fzf-snippets/snippets"}
 PATH=$SNIPPETS_PATH:$PATH
 chmod -R +x $SNIPPETS_PATH/*
 FZF_SNIPPETS_BINDKEY=${FZF_SNIPPETS_BINDKEY:-'^[^['}
